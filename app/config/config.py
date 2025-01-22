253f0e529effbf3ef6f425b53cdb788b24d54a93
unknown <diegoschmied58@gmail.com>
2025-01-22 15:53:18 -0300

    Actualización de archivos

--- app/config/config.py
+++ app/config/config.py
@@ -1,7 +1,7 @@
 # archivo de configuración del sistema.
 
 # versión del TP.
-VERSION = 'Trabajo práctico - COMISIÓN VERANO 2025'
+VERSION = ''
 
 # URL - API
 STUDENTS_REST_API_URL = 'https://hp-api.herokuapp.com/api/characters/students'
